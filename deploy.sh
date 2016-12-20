c3de66ac482eb1655d6729eb6019990fb2a1f83d
Pavel Gurov <pegurov@avito.ru>
2016-12-20 18:42:33 +0300

    changed owner

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -O https://raw.githubusercontent.com/pashaisthebest/protools-ios-deploy-pods/master/_deploy.sh
+curl -O https://raw.githubusercontent.com/motylevm/protools-ios-deploy-pods/master/_deploy.sh
 sh _deploy.sh
 rm -f _deploy.sh
