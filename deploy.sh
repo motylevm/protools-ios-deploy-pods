defcef83910868fd4f8d9cfd42d3dfe33f10bdf0
Pavel Gurov <pegurov@avito.ru>
2017-03-29 17:36:30 +0300

    Script is now downloaded to another folder to leave current folder in clean git state

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/motylevm/protools-ios-deploy-pods/master/_deploy.sh
-sh _deploy.sh $*
-rm -f _deploy.sh
+mkdir ../temp_C24A86D7-D049-454B-A894-A191FFD41EFD
+curl -o ../temp_C24A86D7-D049-454B-A894-A191FFD41EFD/_deploy.sh -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/motylevm/protools-ios-deploy-pods/master/_deploy.sh
+sh ../temp_C24A86D7-D049-454B-A894-A191FFD41EFD/_deploy.sh $*
+rm -rf ../temp_C24A86D7-D049-454B-A894-A191FFD41EFD
